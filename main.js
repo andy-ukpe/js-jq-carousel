0e07057d27c563b97db579e9ad9c3860fff2f7cb
andy <andy.ukpe@outlook.com>
2020-06-09 15:55:23 +0200

    esercizio completato

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ $(document).ready(
       function(){
         farScorrereLeImmaginiInAvanti ();
       }
-      
     );
+
+      $('.prev').click(
+
+        function(){
+          farScorrereLeImmaginiIndietro();
+        }
+      );
+
   }
 );
 
@@ -15,19 +22,42 @@ $(document).ready(
 // creare una funzione per far scorrere le immagini e i pallini
 function farScorrereLeImmaginiInAvanti(){
 
-  var immagineCorrente = $('.image img.active');
-
+  var immagineCorrente = $('.images > img.active');
+  var bollinoCorrente = $('.nav > i.active');
   // far passare le immagini visualizzate a quella successiva
   immagineCorrente.removeClass('active');
-
-  // far passare le immagini visualizzate a quella successiva
+  bollinoCorrente.removeClass('active');
+  // far muovere le immagini dall'ultima alla prima
   if(immagineCorrente.hasClass('last')){
-    $('.image img.active').addClass('active');
+    $('.images img.first').addClass('active');
+    $('.nav > i.first').addClass('active');
   }
 
   // far passare le immagini visualizzate a quella successiva
   else {
     immagineCorrente.next('img').addClass('active');
+    bollinoCorrente.next('i').addClass('active');
   }
 
-}
+};
+
+function farScorrereLeImmaginiIndietro(){
+
+  var immagineCorrente = $('.images > img.active');
+  var bollinoCorrente = $('.nav > i.active');
+  // far passare le immagini visualizzate a quella successiva
+  immagineCorrente.removeClass('active');
+  bollinoCorrente.removeClass('active');
+  // far muovere le immagini dall'ultima alla prima
+  if(immagineCorrente.hasClass('first')){
+    $('.images img.last').addClass('active');
+    $('.nav > i.last').addClass('active');
+  }
+
+  // far passare le immagini visualizzate a quella successiva
+  else {
+    immagineCorrente.prev('img').addClass('active');
+    bollinoCorrente.prev('i').addClass('active');
+  }
+
+};
